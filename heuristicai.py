65cc185e37b12802d5f5dd62ed412dad66f7d77c
Elia Bhend <bugohoss@me.com>
2022-11-03 16:17:54 +0100

    improved heuristicai

--- heuristicai.py
+++ heuristicai.py
@@ -1,7 +1,8 @@
+from heapq import merge
 import random
-import sys
-
 import game
+import sys
+import numpy as np
 
 # Author:				chrn (original by nneonneo)
 # Date:				11.11.2016
@@ -10,13 +11,47 @@ import game
 UP, DOWN, LEFT, RIGHT = 0, 1, 2, 3
 
 
+def heuristicai_move(board, searches_per_move, search_length):
+    first_moves = [UP, DOWN, LEFT, RIGHT]
+    score = np.zeros(4)
+
+    for first_index in range(4):
+        first_move = first_moves[first_index]
+        first_board, first_valid, first_score = first_move(board)
+
+        if first_valid:
+            first_board = merge(first_board)
+            score[first_index] += first_score
+        else:
+            continue
+
+        for later_moves in range(searches_per_move):
+            move_number = 1
+            search_board = np.copy(first_board)
+            is_valid = True
+
+            while is_valid and move_number < search_length:
+                search_board, is_valid, score = random.choice(search_board)
+                if is_valid:
+                    search_board = merge(search_board)
+                    score[first_index] += score
+                    move_number += 1
+
+    best_move_index = np.argmax(score)
+    best_move = first_moves[best_move_index]
+    final_board, position_valid, _ = best_move(board)
+    return final_board, position_valid
+
+
 def find_best_move(board):
     bestmove = -1
 
     # TODO:
     # Build a heuristic agent on your own that is much better than the random agent.
     # Your own agent don't have to beat the game.
-    bestmove = find_best_move_random_agent()
+    # bestmove = find_best_move_random_agent()
+    bestmove = heuristicai_move(board, [1,4], 4)
+
     return bestmove
 
 
@@ -26,7 +61,7 @@ def find_best_move_random_agent():
 
 def execute_move(move, board):
     """
-    move and return the grid without a new random tile
+    move and return the grid without a new random tile 
 	It won't affect the state of the game in the browser.
     """
 
